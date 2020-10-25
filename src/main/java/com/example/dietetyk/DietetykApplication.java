55f426107b8827d9ee0553a7e55e69548a2e9545
Camil757 <kamilsobura801@gmail.com>
2020-10-25 20:43:34 +0100

    Initial commit

---
+++ src/main/java/com/example/dietetyk/DietetykApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietetyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietetykApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(DietetykApplication.class, args);
+    }
+
+}
