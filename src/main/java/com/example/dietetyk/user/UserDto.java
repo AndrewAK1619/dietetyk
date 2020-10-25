3e41c79998f07a31c0eb985b59851a779cdcc31a
Andrzej Kaminski <andrzej.kaminski0591@gmail.com>
2020-10-26 00:57:40 +0100

    Add User layers

---
+++ src/main/java/com/example/dietetyk/user/UserDto.java
@@ -0,0 +1,15 @@
+package com.example.dietetyk.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+}
