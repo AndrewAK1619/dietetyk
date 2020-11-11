4e7c273d92e831e172344a7ee1cc87571e455985
Andrzej Kaminski <andrzej.kaminski0591@gmail.com>
2020-11-11 21:35:30 +0100

    Add save (UserController / Backend)

--- src/main/java/com/example/dietetyk/user/UserRepository.java
+++ src/main/java/com/example/dietetyk/user/UserRepository.java
@@ -1,10 +1,12 @@
 package com.example.dietetyk.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	Optional<User> findByEmail(String email);
 	List<User> findAllByOrderByIdAsc();
 }
--- src/main/java/com/example/dietetyk/user/role/UserRoleRepository.java
+++ src/main/java/com/example/dietetyk/user/role/UserRoleRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
+	UserRole findByRole(String role);
 }
