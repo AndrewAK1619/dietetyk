dbd689a8fdd1903513efa5081e83199f45dd2929
Camil757 <kamilsobura801@gmail.com>
2021-02-07 14:53:14 +0100

    update meal endpoint

--- src/main/java/com/example/dietetyk/meal/MealController.java
+++ src/main/java/com/example/dietetyk/meal/MealController.java
@@ -12,7 +12,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/meals")
+@RequestMapping("/admin/meals")
 public class MealController {
 
     private MealService mealService;
